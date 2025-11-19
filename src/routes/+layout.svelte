5a10698c2dfdaaa7d8365ee24de9d1670718c98e
martin-schaefer <martin.schaefer.14@outlook.com>
2025-11-19 14:44:09 +0100

    Added tabbed navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,28 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
 	import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
+	import Tab, { Icon, Label } from '@smui/tab';
+	import TabBar from '@smui/tab-bar';
+
+	let tabs = [
+		{
+			icon: 'access_time',
+			label: 'Recents',
+		},
+		{
+			icon: 'near_me',
+			label: 'Nearby',
+		},
+		{
+			icon: 'favorite',
+			label: 'Favorites',
+		},
+		{
+			icon: 'github',
+			label: 'Woof',
+		},
+	];
+	let active = $state(tabs[0]);
 	let { children } = $props();
 </script>
 
@@ -8,12 +30,23 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 <header>
-	<Row>
-		<TopAppBar>
-			<Section>
-				<Title>Bitzer</Title>
-			</Section>
+		<TopAppBar style="background-color: #212121;">
+			<Row>
+				<Section>
+					<Title>Bitzer</Title>
+				</Section>
+				<Section>
+					<TabBar {tabs} key={(tab) => tab.label} bind:active>
+						{#snippet tab(tab)}
+							<Tab {tab}>
+								<Icon class="material-icons">{tab.icon}</Icon>
+								<Label>{tab.label}</Label>
+							</Tab>
+						{/snippet}
+					</TabBar>
+				</Section>
+			</Row>
 		</TopAppBar>
-	</Row>
 </header>
 {@render children()}
+
