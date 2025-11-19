6f3d7a54abea9f8e3c306246cd3be364ac7b7e93
martin-schaefer <martin.schaefer.14@outlook.com>
2025-11-19 14:15:33 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+	import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+<header>
+	<Row>
+		<TopAppBar>
+			<Section>
+				<Title>Bitzer</Title>
+			</Section>
+		</TopAppBar>
+	</Row>
+</header>
+{@render children()}
